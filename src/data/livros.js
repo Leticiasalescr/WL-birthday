a3b7a962b58d885600b059d3828e5512dfe058b0
Letícia Sales <leticiafersales@gmail.com>
2026-05-31 21:58:56 -0300

    fix: caminhos das imagens

--- src/data/livros.js
+++ src/data/livros.js
@@ -2,36 +2,31 @@ export const livros = [
   {
     id: 1,
     name: "O Retrato de Dorian Gray",
-    image: "/public/images/dorian.jpg",
+    image: "/images/dorian.jpg",
     price: "R$ 40",
-    description:
-      "Tem que ser da edição da Penguin kkkk",
+    description: "Tem que ser da edição da Penguin kkkk",
     priority: "☆☆☆☆☆"
   },
-
   {
     id: 2,
     name: "A Elegância do Ouriço",
-    image: "/public/images/ouriço.jpg",
+    image: "/images/ouriço.jpg",
     price: "R$ 55",
-    description:
-      "Ansiosa para ler esse romancee!!! quero demais",
+    description: "Ansiosa para ler esse romancee!!! quero demais",
     priority: "☆☆☆☆☆"
   },
-
   {
     id: 3,
     name: "Uma Vida Pequena",
-    image: "/public/images/umavida.jpg",
+    image: "/images/umavida.jpg",
     price: "R$ 80",
-    description:
-      "Deve ter uns dois anos que está na minha lista lkkkkk preciso ler",
+    description: "Deve ter uns dois anos que está na minha lista lkkkkk preciso ler",
     priority: "☆☆☆☆☆"
   },
   {
     id: 4,
     name: "Ensaio Sobre a Cegueira",
-    image: "/public/images/ensaio.jpg",
+    image: "/images/ensaio.jpg",
     price: "R$ 45",
     description: "Tá na minha lista parece interessante",
     priority: "☆☆☆☆☆"
@@ -39,7 +34,7 @@ export const livros = [
   {
     id: 5,
     name: "Admirável Mundo Novo",
-    image: "/public/images/mundo.jpg",
+    image: "/images/mundo.jpg",
     price: "R$ 50",
     description: "Quero ler esse clássico da distopia",
     priority: "☆☆☆☆☆"
@@ -47,7 +42,7 @@ export const livros = [
   {
     id: 6,
     name: "A morte de Ivan Ilitch",
-    image: "/public/images/ivan.jpg",
+    image: "/images/ivan.jpg",
     price: "R$ 40",
     description: "Quero ler Tolstói",
     priority: "☆☆☆☆☆"
